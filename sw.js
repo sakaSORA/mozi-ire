ca471263f3705f52a367c66123492c543255d3be
sakaSORA <g.saka.g@gmail.com>
2025-12-30 19:13:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-const CACHE_NAME = 'text-editor-v2';
+const CACHE_NAME = 'text-editor-v2.1';
 const ASSETS = [
   './',
-  './index.html', // あなたのHTMLファイル名に合わせて変更してください
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js',
   'https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=DotGothic16&family=Kaisei+Tokumin:wght@700&family=Kiwi+Maru:wght@500&family=M+PLUS+1p:wght@800&family=Sawarabi+Mincho&display=swap'
 ];
 
@@ -9,6 +14,18 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
